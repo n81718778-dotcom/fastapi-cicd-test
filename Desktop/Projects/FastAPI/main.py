8f3d2b19861018f2fd42d307a205fa29afb830df
Alex <n81718778@gmail.com>
2026-04-18 13:39:34 +0300

    Trigger action

--- Desktop/Projects/FastAPI/main.py
+++ Desktop/Projects/FastAPI/main.py
@@ -4,4 +4,4 @@ app = FastAPI()
 
 @app.get("/")
 def read_root():
-    return {"message": "Привет! Мой Docker-контейнер работает!"}
+    return {"message": "Привет! Мой Docker-контейнер работает!!"}
